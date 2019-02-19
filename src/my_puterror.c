2c3ca977f893fdb16656fa43c36f690bbe27d262
Elias <30868938+Makoto7@users.noreply.github.com>
2019-02-19 14:05:55 +0100

    Update my_puterror.c

--- src/my_puterror.c
+++ src/my_puterror.c
@@ -1,13 +1,3 @@
-/*
-** my_puterror.c for my_puterror in /home/abouss_e/rendu/Cprog/CPE_2015_BSQ
-**
-** Made by Elias ABOUSSAIR
-** Login   <abouss_e@epitech.net>
-**
-** Started on  Thu Aug 11 18:55:04 2016 Elias ABOUSSAIR
-** Last update Thu Aug 11 18:56:29 2016 Elias ABOUSSAIR
-*/
-
 #include "bsq.h"
 
 void	my_puterror(void)
